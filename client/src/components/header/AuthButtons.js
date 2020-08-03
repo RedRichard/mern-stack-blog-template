6908f2ee6e6e407d39bfdd4ed3f14acc01da79ff
Ricardo HG <gomric@hotmail.com>
2020-08-03 17:28:51 -0500

    Update: AuthButtons component ready

--- client/src/components/header/AuthButtons.js
+++ client/src/components/header/AuthButtons.js
@@ -1,23 +1,29 @@
-import React, { Component } from "react";
+import React, { useContext } from "react";
+import UserContext from "../context/UserContext";
 
 import SignUpButton from "./SignUpButton";
 import LogInButton from "./LogInButton";
 import LogOutButton from "./LogOutButton";
 
-export default class AuthButtons extends Component {
-  render() {
-    return (
-      <>
-        <li className="navbar-item auth-button">
-          <SignUpButton />
-        </li>
-        <li className="navbar-item auth-button">
-          <LogInButton />
-        </li>
+export default function AuthButtons() {
+  let { userData } = useContext(UserContext);
+
+  return (
+    <>
+      {userData.user ? (
         <li className="navbar-item auth-button">
           <LogOutButton />
         </li>
-      </>
-    );
-  }
+      ) : (
+        <>
+          <li className="navbar-item auth-button">
+            <SignUpButton />
+          </li>
+          <li className="navbar-item auth-button">
+            <LogInButton />
+          </li>{" "}
+        </>
+      )}
+    </>
+  );
 }
